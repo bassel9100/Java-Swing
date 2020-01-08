431bee8f996a32a5624cdd0bb0800de2f4d65c5f
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-08 19:02:45 +0100

    Minor changes in other file

--- Chapter 1/src/SwimPoints.java
+++ Chapter 1/src/SwimPoints.java
@@ -1,2 +1,29 @@
+import javax.swing.*;
+
 public class SwimPoints {
+    public static void main(String[] args){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int result = 0;
+        int final_result;
+        int difficulty;
+
+        difficulty = Integer.parseInt(JOptionPane.showInputDialog("Please enter difficulty level"));
+
+        for(int i = 1; i <= 7; i++){
+            String str = JOptionPane.showInputDialog("Enter point for judge nr: " + i);
+            result = result + Integer.parseInt(str);
+            if(result > max) {
+                max = result;
+            }
+            if(result < min) {
+                min = result;
+            }
+        }
+
+        result = result - max - min;
+        System.out.println(min + " " + max + " " + result);
+        final_result = result/(5*3*difficulty);
+        JOptionPane.showMessageDialog(null, "The final point is " + final_result);
+    }
 }
