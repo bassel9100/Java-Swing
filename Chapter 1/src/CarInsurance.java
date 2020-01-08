dd9f943f550e3f41467638638c40eac75961f846
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-08 15:33:51 +0100

    Updated verion of file

--- Chapter 1/src/CarInsurance.java
+++ Chapter 1/src/CarInsurance.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class CarInsurance {
+    public static void main(String[] args){
+        //Variables
+        int current_year;
+        int car_model_year;
+        //Variable Declarations
+        current_year = Integer.parseInt(JOptionPane.showInputDialog("What year is it today?"));
+        car_model_year = Integer.parseInt(JOptionPane.showInputDialog("What year was the car manufactured?"));
+        int r = current_year - car_model_year;
+        //Comparing final answer
+        if(r < 5)
+            JOptionPane.showMessageDialog(null, "You will need a half insurance though the car is new.");
+         else if(r > 25)
+            JOptionPane.showMessageDialog(null, "You will need a veteran insurance");
+         else
+            JOptionPane.showMessageDialog(null, "You will need a full insurance");
+    }
 }
