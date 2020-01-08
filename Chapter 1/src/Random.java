43b018acc538267341a009121f87fa7b25198a3b
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-08 13:38:38 +0100

    N

--- Chapter 1/src/Random.java
+++ Chapter 1/src/Random.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Random {
+    public static void main(String[] args){
+        String output = "";
+        String input = JOptionPane.showInputDialog("How many times do you want to loop?");
+        int loop = Integer.parseInt(input);
+        for(int i = 0; i < loop; i++){
+            for(int j = 0; j <=i; j++)
+                output = output + "x";
+        output += '\n';
+        }
+        JOptionPane.showMessageDialog(null, output);
+    }
 }
