930a5660c689a1df3556efc0b677d7e29c5cdeb9
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-08 15:47:50 +0100

    Editet sphere file

--- Chapter 1/src/Sphere.java
+++ Chapter 1/src/Sphere.java
@@ -4,11 +4,15 @@ public class Sphere {
     public static void main(String[] args){
         double r;
         double volume, area;
+        while(true) {
+            r = Double.parseDouble(JOptionPane.showInputDialog("Enter radius value"));
+            if(r < 0){
+                r = Double.parseDouble(JOptionPane.showInputDialog("Radius cannot be negative. Enter value greater than 0"));
+            }
+            volume = (4 * Math.PI * r * r * r) / 3;
+            area = 4 * Math.PI * r * r;
 
-        r = Double.parseDouble(JOptionPane.showInputDialog("Enter radius value"));
-        volume = (4*Math.PI*r*r*r)/3;
-        area = 4*Math.PI*r*r;
-
-        JOptionPane.showMessageDialog(null, "The Volume of the Sphere is: " + volume + '\n' + "The Area of the Sphere is: " + area);
+            JOptionPane.showMessageDialog(null, "The Volume of the Sphere is: " + volume + '\n' + "The Area of the Sphere is: " + area);
+        }
     }
 }
