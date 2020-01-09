8c2b94f8c388fa248288fb7520e5278f0c718662
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-09 14:36:05 +0100

    Added another solutions to problems from Chapter 3

---
+++ Chapter 3/src/Elevator.java
@@ -0,0 +1,20 @@
+public class Elevator {
+    private static int upp = 1;
+    private static int still = 0;
+    private static int down = -1;
+
+    private int direction;
+    private int floorLevel;
+
+    public int getDirection(){
+        return direction;
+    }
+
+    public int getFloorLevel(){
+        return floorLevel;
+    }
+
+    public void goTo(int floorLevel){
+
+    }
+}
---
+++ Chapter 3/src/Functions.java
@@ -0,0 +1,18 @@
+public class Functions {
+    
+    public static double median(int x, int y){
+        return (x + y) / 2;
+    }
+
+    public static double quadSum(int x, int y){
+        return (x * x) + (y * y;
+    }
+
+    public static boolean sharedFactor(int x, int y){
+        int k = 2;
+        while(k <= x && k <= y){
+            if(x % k == 0 && y % k ==0)
+                return true;
+        }  return false;
+    }
+}
