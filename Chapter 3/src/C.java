5e521e37368b5af200130dbcb67601c4937a0772
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-09 21:58:26 +0100

    Added problem 1 and 2

---
+++ Chapter 3/src/C.java
@@ -0,0 +1,17 @@
+public class C {
+    private static int count;
+    private int id;
+
+    public C(){
+        count++;
+        id = count;
+    }
+
+    public static int getCount(){
+        return count;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
