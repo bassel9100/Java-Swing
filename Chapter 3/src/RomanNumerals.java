3ab7a5d1075020524acf4b161ebf82b8a89ce69b
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-10 13:40:32 +0100

    Problem 9

---
+++ Chapter 3/src/RomanNumerals.java
@@ -0,0 +1,7 @@
+public class RomanNumerals {
+    public enum RomanNume{
+        I(1), V(5), X(10), L(50), C(100), D(500), M(1000);
+        public final int value;
+        RomanNume(int v){value = v;}
+    }
+}
