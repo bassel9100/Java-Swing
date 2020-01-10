7d2e11ccd45e377d4439bc68ab54cb387af1607e
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-10 14:18:07 +0100

    Notthing important

--- Chapter 3/src/Random.java
+++ Chapter 3/src/Random.java
@@ -3,6 +3,17 @@ import java.util.*;
 import static java.lang.Math.*;
 
 public class Random {
+
+    public static boolean equal(int[] arr1, int[] arr2){
+        if(arr1.length != arr2.length)
+            return false;
+        for(int i = 0; i < arr1.length; i++){
+            if(arr1[i] == arr2[i])
+                return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args){
         double x = E;
         double y = PI;
