684139d331ee2891b94e6484255c3341f50e6f9f
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-12 16:30:39 +0100

    Solved another problem from chaper 5

--- Chapter 5/src/carRent.java
+++ Chapter 5/src/carRent.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.*;
+import javax.swing.*;
 
 public class carRent {
+    public static void main(String[] args){
+        int days;
+        double pricePerDay;
+        String carName;
+        while(true){
+            String str = JOptionPane.showInputDialog("Enter days, price perDay and car model");
+            if(str == null){
+                JOptionPane.showMessageDialog(null, "No input try again");
+                break;
+            }
+            Scanner sc = new Scanner(str);
+            try{
+                 days = sc.nextInt();
+                 pricePerDay = sc.nextDouble();
+                 carName = sc.next();
+                JOptionPane.showMessageDialog(null, "Total price for renting the " + carName + " is: " + (days*pricePerDay));
+            } catch(InputMismatchException e){
+                JOptionPane.showMessageDialog(null, "Wrong type format try agian");
+            } catch(NoSuchElementException e){
+                JOptionPane.showMessageDialog(null, "No input try again");
+            }
+        }
+    }
 }
