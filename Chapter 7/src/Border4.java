57dc50bc2d7b310f43e372198d3ceca81283e982
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-18 14:00:00 +0100

    Last problems from chapter 7 and some from chaper 8

--- Chapter 7/src/Border4.java
+++ Chapter 7/src/Border4.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
-public class Border4 {
+public class Border4 extends JFrame {
+
+    public Border4() {
+        LineBorder b1 = new LineBorder(Color.yellow, 5);
+        LineBorder b2 = new LineBorder(Color.orange, 5);
+        LineBorder b3 = new LineBorder(Color.red, 5);
+        LineBorder b4 = new LineBorder(Color.magenta, 5);
+        JLabel l = new JLabel("Hej", JLabel.CENTER);
+        l.setBorder(new CompoundBorder(new CompoundBorder(new CompoundBorder(b4, b3), b2), b1));
+        add(l);
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main (String[] arg) {
+        Border4 b = new Border4();
+    }
 }
--- Chapter 7/src/SimpleFrame.java
+++ Chapter 7/src/SimpleFrame.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class SimpleFrame {
+public class SimpleFrame extends JFrame implements ActionListener {
+    private JButton close = new JButton("X");
+
+    public SimpleFrame() {
+        setUndecorated(true);
+        Container c = (Container) getContentPane();
+        JPanel top = new JPanel();
+        top.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        top.add(close);
+        close.setBackground(Color.lightGray);
+        c.add(top, BorderLayout.NORTH);
+        JLabel l = new JLabel("Close with button", JLabel.CENTER);
+        c.add(l, BorderLayout.CENTER);
+        close.addActionListener(this);
+        setOpacity(0.5f);
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == close)
+            System.exit(0);
+    }
+
+    public static void main (String[] arg) {
+        SimpleFrame f = new SimpleFrame();
+    }
 }
