d9e30e5fa83467711eeb029ddd1b3010bf8059e6
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-19 12:39:51 +0100

    More problems from chapter 9

---
+++ Chapter 9/src/Person.java
@@ -0,0 +1,54 @@
+public class Person {
+
+    protected String name, address;
+    protected int age;
+    private Person spouse;
+
+    public Person(){};
+
+    public Person(String name){this.name = name;}
+
+    public Person(String name, String address, int age){
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void married(Person p){
+        spouse = p;
+        p.spouse = this;
+    }
+
+    public void divorce(){
+        spouse.spouse = null;
+        spouse = null;
+    }
+
+    public Person getSpouse(){
+        return spouse;
+    }
+}
---
+++ Chapter 9/src/Subject.java
@@ -0,0 +1,20 @@
+public class Subject {
+    String courseName;
+    int courseCode;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(int courseCode) {
+        this.courseCode = courseCode;
+    }
+}
