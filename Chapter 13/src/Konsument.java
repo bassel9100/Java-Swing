46922ef3c2365928da9c1cf2023cfdf5c864ce1e
Bassel Kanaan <bassel.9100@gmail.com>
2020-01-25 12:51:16 +0100

    Another problem from chapter 13

--- Chapter 13/src/Konsument.java
+++ Chapter 13/src/Konsument.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class Konsument implements Runnable{
+    public Thread activity = new Thread(this);
+    private long duration;
+    private SimpleQueue q;
 
-public class Konsument {
+    public Konsument(long duration, SimpleQueue q){
+        this.duration = duration * 1000;
+        this.q = q;
+    }
+
+    public void run(){
+        while(!Thread.interrupted()){
+            try{
+                Thread.sleep(duration);
+                System.out.println(q.take() + " ");
+            } catch(InterruptedException e){
+                break;
+            }
+        }
+    }
 }
