928bc257106a958c3fe6822f84643206c67c8709
Bassel Kanaan <bassel.9100@gmail.com>
2020-02-08 14:40:22 +0100

    Minor changes to several problem from chapter 3

--- Chapter 3/src/SGD.java
+++ Chapter 3/src/SGD.java
@@ -1,20 +1,24 @@
 import javax.swing.*;
-
+//Find the greatest common divider for two integers x and y
 public class SGD {
 
     public static int findsSGD(int x, int y){
-        int remainder = x % y;
-        while(remainder != 0){
+        int r = x % y;
+        while(r != 0){
             x = y;
-            y = remainder;
-            remainder = x % y;
+            y = r;
+            r = x % y;
         }
         return y;
     }
 
-    public static void main(String[] args){
+    void program(){
         int x = Integer.parseInt(JOptionPane.showInputDialog("Enter first number"));
         int y = Integer.parseInt(JOptionPane.showInputDialog("Enter second number"));
         JOptionPane.showMessageDialog(null, "The highest common divisor is: " + findsSGD(x,y));
+    }
+
+    public static void main(String[] args){
+        new SGD().program();
     }
 }
