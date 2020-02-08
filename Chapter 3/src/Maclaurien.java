27aa42996e504bb9ce4fbde481ebff9f86dcb561
Bassel Kanaan <bassel.9100@gmail.com>
2020-02-08 15:04:34 +0100

    Change to problem 6 from chapter 2

--- Chapter 3/src/Maclaurien.java
+++ Chapter 3/src/Maclaurien.java
@@ -1,32 +1,42 @@
+import java.util.*;
+
 public class Maclaurien {
 
-    public static double factorial(double x){
-        double sum = 1;
-        while(x > 0)
-            sum *= x--;
-        return sum;
-    }
-
-    public static double abs(double x){
-        if(x > 0)
-            return x;
-        return -x;
-    }
-
-    public static double sin(double x){
-        int k = 1;
-        double term = x;
-        double sum = 0;
-
-        while(abs(term) >= 1.0e-5){
-            sum += term;
-            k += 2;
-            term = -(Math.pow(term,k))/factorial(k);
+    public static int factorial(int x){
+        int sum = 1;
+        while(x > 0) {
+            sum *= x;
+            x--;
         }
         return sum;
     }
 
+    public static double abs(double x){
+        if(x < 0)
+            return -x;
+        else
+            return x;
+    }
+
+    public static double sin(double x){
+        double sum = 0;
+        double term = 1;
+        int k = 1;
+        while(abs(term) >= 10e-5){
+            sum += term;
+            k += 2;
+            term = -term*x*x/(k-1)/k;
+        }
+        return sum;
+    }
+
+    void program(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter you sin value :> ");
+        System.out.println(sin(sc.nextDouble()));
+    }
+
     public static void main(String[] args){
-        System.out.println(sin(2));
+        new Maclaurien().program();
     }
 }
