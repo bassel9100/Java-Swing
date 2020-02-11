88880412b1da5f8e85129b150db23e4374bddcc8
Bassel Kanaan <bassel.9100@gmail.com>
2020-02-11 12:20:21 +0100

    Minor changes for Stapel Demo from chapter 7

--- Chapter 7/src/StapelDemo.java
+++ Chapter 7/src/StapelDemo.java
@@ -7,13 +7,16 @@ public class StapelDemo extends JFrame{
     public StapelDemo(){
         Stapel s1 = new Stapel(true);
         Stapel s2 = new Stapel(true);
+        Stapel s3 = new Stapel(true);
         s1.setPercent(30); s1.setBorder(new LineBorder(Color.LIGHT_GRAY));
         s2.setPercent(75); s2.setBorder(new LineBorder(Color.LIGHT_GRAY));
+        s3.setPercent(95); s3.setBorder(new LineBorder(Color.LIGHT_GRAY));
         s1.setForeground(Color.RED); s1.setBackground(Color.BLACK);
         s2.setForeground(Color.RED); s2.setBackground(Color.BLACK);
-        setLayout(new GridLayout(1,2));
-        add(s1); add(s2);
-        setSize(50,300);
+        s3.setForeground(Color.RED); s3.setBackground(Color.BLACK);
+        setLayout(new GridLayout(1,3));
+        add(s1); add(s2); add(s3);
+        setSize(70,300);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
