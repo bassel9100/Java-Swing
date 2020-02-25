f6286a13e26190394ca77661f8ee6a921fb88f83
Bassel Kanaan <bassel.9100@gmail.com>
2020-02-25 19:50:02 +0100

    Updated problem 1 from chapter 16

--- Chapter 16/src/Secret.java
+++ Chapter 16/src/Secret.java
@@ -1,43 +1,44 @@
-import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Secret {
 
-    public static int number(char c){
-        c = Character.toLowerCase(c);
-        if(c >= 'a' && c <= 'z')
-            return (int) c - (int) 'a';
-        else if(c == 'å' || c == 'Å')
-            return 26;
-        else if(c == 'ä' || c == 'Ä')
-            return 27;
-        else if(c == 'ö' || c == 'Ö')
-            return 28;
-        return -1;
-    }
+    public static void main(String[] args) throws IOException {
+        Secret n1 = new Secret();
+        System.out.println("Skriv in kodningstabellen");
+        System.out.println("abcdefghijklmnopqrstuvwxyzåäö");
+        char[] tab = new char[29];
+        for (char c = 'a'; c <= 'z'; c++)
+            tab[n1.number((char) System.in.read())] = c;
+        tab[n1.number((char) System.in.read())] = 'å';
+        tab[n1.number((char) System.in.read())] = 'ä';
+        tab[n1.number((char) System.in.read())] = 'ö';
 
-    public static void main(String[] args) throws IOException{
-        InputStreamReader  dosIn  = new InputStreamReader(System.in, "Cp850");
-        PrintWriter        dosOut = new PrintWriter(new OutputStreamWriter(System.out, "Cp850"));
-        System.out.println("Write in you secret code");
-        dosOut.println("abcdefghijklmnopqrstuvwxyzåäö");
-        dosOut.flush();
-        char[] arr = new char[29];
-        for (char c='a'; c<='z'; c++)
-            arr[number((char) System.in.read())]= c;
-        arr[number((char) System.in.read())]= 'å';
-        arr[number((char) System.in.read())]= 'ä';
-        arr[number((char) System.in.read())]= 'ö';
-
-        FileReader file = new FileReader("secret.txt");
+        FileReader fil = new FileReader("secret.txt");
         int i;
-        while((i = file.read()) != -1){
+        while ((i = fil.read()) != -1) {
             char c = (char) i;
-            int n = number(c);
-            if(n >= 0)
-                System.out.print(arr[n]);
+            int n = n1.number(c);
+            if (n >= 0)
+                System.out.print(tab[n]);
             else
                 System.out.print(c);
             System.out.flush();
         }
+    }
+
+    public int number(char c){
+        c = Character.toLowerCase(c);
+        if(c >= 'a' && c <= 'z'){
+            return (int) c - (int) 'a'; //a = 97 .. z = 122
+        } else if(c == 'a' || c == 'Ä'){
+            return 26;
+        } else if(c == 'å' || c == 'Å'){
+            return 27;
+        } else if(c == 'ö' || c == 'Ö'){
+            return 28;
+        }
+        return -1;
     }
 }
